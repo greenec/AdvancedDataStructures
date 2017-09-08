f2fd66b67f1e25c30b2cf1e5d5949e72fd94ccee
Connor Greene <connor.greene2000@gmail.com>
2017-09-08 14:14:07 -0400

    Started writing All-Star Cell

--- AdvancedDataStructures/Program.cs
+++ AdvancedDataStructures/Program.cs
@@ -10,7 +10,30 @@ namespace AllStarCell
     {
         static void Main(string[] args)
         {
+            String op;
 
+            String x = Console.ReadLine();
+            String y = Console.ReadLine();
+            String z = Console.ReadLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                String[] ops = Console.ReadLine().Split(' ');
+
+                for (int j = ops.Length - 1; j > 0; j--)
+                {
+                    op = ops[j];
+
+                    if (op.Length == 1)
+                    {
+                        i--;
+                    }
+                    else if (op.Equals("DIV"))
+                    {
+
+                    }
+                }
+            }
         }
     }
 }
